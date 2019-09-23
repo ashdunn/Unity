f32fc5e9e6e11f85d35d24cdabce5a641d253f37
ashdunn <31973458+ashdunn@users.noreply.github.com>
2019-09-22 22:53:25 -0400

    transition between run and walk

--- Assets/SimpleMovement.cs
+++ Assets/SimpleMovement.cs
@@ -7,13 +7,22 @@ using UnityEngine;
 public class SimpleMovement : MonoBehaviour
 {
 
-    public float Speed;
+    public float Speed = 1.5f;
     public float RotateSpeed;
+    Animator anim;
+    public float runSpeed = 8.0f;
+    public float walkSpeed = 1.5f;
+    private bool running = false;
+
+    int runHash = Animator.StringToHash("Running");
+    int walkHash = Animator.StringToHash("Walking");
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        anim.SetTrigger(walkHash);
     }
 
     // Update is called once per frame
@@ -21,10 +30,40 @@ public class SimpleMovement : MonoBehaviour
     {
         CharacterController controller = GetComponent<CharacterController>();
 
+        float v = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Walk", v);
+
         transform.Rotate(0, Input.GetAxis("Horizontal") * RotateSpeed, 0);
         Vector3 forward = transform.TransformDirection(Vector3.forward);
 
-        float curSpeed = Speed * Input.GetAxis("Vertical");
+        float curSpeed = Speed * v;
         controller.SimpleMove(forward * curSpeed);
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if (!running)
+            {
+                
+                anim.SetBool("Running", true);
+                running = true;
+                Speed = runSpeed;
+               
+            }
+            else
+            {
+                running = false;
+                anim.SetBool("Running", false);
+                Speed = walkSpeed;
+            }
+        }
+
+        if (running && Input.GetKey(KeyCode.W))
+            Speed = runSpeed;
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            Speed = walkSpeed;
+        }
     }
 }
