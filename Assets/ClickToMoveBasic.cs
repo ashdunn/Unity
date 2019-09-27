ebc7587a1e699111f71f945be59884ac48a8ccd0
ashdunn <31973458+ashdunn@users.noreply.github.com>
2019-09-27 19:06:43 -0400

    part two can run but falls halfway through floor

--- Assets/ClickToMoveBasic.cs
+++ Assets/ClickToMoveBasic.cs
@@ -5,10 +5,14 @@ public class ClickToMoveBasic : MonoBehaviour
 {
     RaycastHit hitInfo = new RaycastHit();
     UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
 
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("run", false);
+        agent.speed = 2;
     }
     void Update()
     {
@@ -16,7 +20,20 @@ public class ClickToMoveBasic : MonoBehaviour
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
-                agent.destination = hitInfo.point;
+            {
+                if (Vector3.Distance(hitInfo.point, agent.destination) < .5)
+                {
+                    print("kslfjslfjs");
+                    anim.SetBool("run", true);
+                    agent.speed = 5;
+                }
+                else
+                {
+                    anim.SetBool("run", false);
+                    agent.destination = hitInfo.point;
+                    agent.speed = 2;
+                }
+            }
         }
     }
 }
